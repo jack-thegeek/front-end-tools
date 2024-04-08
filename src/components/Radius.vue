70bb2ef8e371ff3960d50ef07da251d61880bcd3
jackson <jacksonhe@vectorbuilder.net>
2024-04-08 17:36:26 +0800

    修正自动计算

--- src/components/Radius.vue
+++ src/components/Radius.vue
@@ -1,19 +1,70 @@
 <script setup>
-import { ref } from 'vue'
+import {computed, ref} from 'vue'
 
-const wheelColor = ref('#e6f5eb')
+const outerR = ref(65)
+const innerR = ref(30)
+const padding = ref(`${outerR.value - innerR.value}`)
+
+// 修改外圆角，保持内边距不变，自动修正内圆角
+// 修改内边距，保持外圆角不变，自动修正内圆角
+function fixInnerR() {
+    let fix = Number(outerR.value) - Number(padding.value);
+    innerR.value = 0 <= fix && fix <= 100 ? fix : 0;
+}
+
+// 修改内圆角，保持内边距不变，自动修正外圆角
+function fixOuterR() {
+    let fix = Number(innerR.value) + Number(padding.value);
+    outerR.value = 0 <= fix && fix <= 100 ? fix : 100;
+}
+
 </script>
 
 <template>
     <div class="container">
         <div class="stage">
-            <div class="left"></div>
-            <div class="right"></div>
+            <div class="left">
+                <div :style="{ borderRadius: `${outerR}px`, padding: `${padding}px` }" class="outer-box">
+                    <div :style="{ borderRadius: `${innerR}px` }" class="inner-box"></div>
+                </div>
+            </div>
+            <div class="right">
+                <div class="space-between">
+                    <div>外圆角</div>
+                    <div>{{ outerR }}px</div>
+                </div>
+                <input type="range" :min="0" :max="100" v-model="outerR" @input="fixInnerR">
+
+                <div class="space-between">
+                    <div>内圆角</div>
+                    <div>{{ innerR }}px</div>
+                </div>
+                <input type="range" :min="0" :max="100" v-model="innerR" @input="fixOuterR">
+
+                <div class="space-between">
+                    <div>内边距</div>
+                    <div>{{ padding }}px</div>
+                </div>
+                <input type="range" :min="0" :max="100" v-model="padding" @input="fixInnerR">
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
+.outer-box {
+    width: 60%;
+    height: 60%;
+    border: 3px solid #aec6e2;
+}
+
+.inner-box {
+    width: 100%;
+    height: 100%;
+    background-color: #e4ebf5;
+    border: 3px solid #cfddee;
+}
+
 .container {
     width: 100%;
     height: 100%;
@@ -26,7 +77,7 @@ const wheelColor = ref('#e6f5eb')
 }
 
 .stage {
-    width: 100%;
+    width: 90%;
     display: flex;
     gap: 40px;
 }
@@ -37,8 +88,29 @@ const wheelColor = ref('#e6f5eb')
     height: 420px;
     padding: 1rem;
     border-radius: 1rem;
-    background-color: aliceblue;
+    background-color: #fafafa;
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, .1), 0 2px 4px -2px rgba(0, 0, 0, .1);
+}
+
+.left {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.right {
+    padding: 4rem;
+
+    input {
+        width: 100%;
+        display: block;
+        margin-bottom: 2rem;
+    }
+}
+
+.space-between {
+    display: flex;
+    justify-content: space-between;
 }
 
 @media (max-width: 700px) {
